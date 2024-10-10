e7dfa5e9f41a92c145aa998344995bf023c38165
lamikayko123 <butligarchie@gmail.com>
2024-10-10 09:04:36 +0800

    haha

---
+++ nestedtask.py
@@ -0,0 +1,32 @@
+names= [
+    ['Alice','Bob','Charlie'], 
+    ['David','Eve','Frank'],
+    ['Grace','Heidi','Ivan'],
+    ['judy','Ken','Laura']
+]
+# Check if "Alice" exist and remove her
+
+Alice_found = False
+
+for list in names:
+    if "Alice" in list:
+        list.remove("Alice")
+    Alice_found = True
+
+
+if Alice_found:
+    new_name = input("Alice found. Enter a new to add: ")
+    names[0].append(new_name)
+
+    print(names)
+
+    for list in names:
+        for name in list:
+            print(name)
+    
+
+
+
+
+
+        
